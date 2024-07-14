2f49ea048395da83b582ea8c5417c184c8710cb5
swostika9988 <sostikamahato40@gmail.com>
2024-07-14 15:14:59 +0545

    udpate

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Movies,Genres
+from .models import User,Movies,Genres,Trendingmovie
 
 
 # Register your models here.
@@ -19,4 +19,9 @@ admin.site.register(Genres)
 class GeneresAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+admin.site.register(Trendingmovie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'poster_url', 'trailer_url', 'rating', 'release_date')
+    search_fields = ('title',)    
    
